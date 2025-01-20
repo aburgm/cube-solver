464914e0ac0490803e4a4b14b34900866b857cb2
Armin Burgmeier <armin@arbur.net>
2025-01-20 23:02:46 +0100

    Print solution

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use cube_solver::{Placement, Solver, State};
 
+fn print_track(cube_track: Vec<(State, Placement)>) {
+    for (i, (_, placement)) in cube_track.iter().enumerate() {
+        println!("{}: {:?}", i, placement);
+    }
+}
+
 fn main() {
     let solver = Solver::new();
     let mut cube_track: Vec<(State, Placement)> = vec![Default::default()];
 
+    // TODO(armin): print speed somewhere
     while !cube_track.is_empty() {
         let len = cube_track.len();
         if let Some((new_state, placement)) = solver.next_candidate(&mut cube_track[len - 1].0) {
@@ -13,6 +20,7 @@ fn main() {
             if Solver::is_complete(&new_state) {
                 // we found a winner
                 println!("We found a solution!");
+                print_track(cube_track);
                 return;
             }
 
@@ -23,22 +31,22 @@ fn main() {
             }
 
             cube_track.push((new_state, Placement::default()));
-            if cube_track.len() < 10 {
+            /*if cube_track.len() < 10 {
                 println!(
                     "Found a new candidate, now having {}; first placement={:?}",
                     cube_track.len(),
                     cube_track[0].1,
                 );
-            }
+            }*/
         } else {
             cube_track.pop();
-            if cube_track.len() < 10 {
+            /*if cube_track.len() < 10 {
                 println!(
                     "Iterated all candidates, now having {}; first placement={:?}",
                     cube_track.len(),
                     cube_track[0].1,
                 );
-            }
+            }*/
         }
     }
 
